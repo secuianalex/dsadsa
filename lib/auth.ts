9279b143422c60d8a2c3836e3e1511c59d2b2222
secuianalex <secuianalex@gmail.com>
2025-08-31 14:30:09 +0300

    Fix NextAuth configuration by adding secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,6 +10,7 @@ const prisma = new PrismaClient()
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: "credentials",
